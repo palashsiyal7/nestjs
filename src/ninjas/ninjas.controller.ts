8d989fc0fb1892fdd6663dd86f8a5bee4ded84fe
palashsiyal7 <palash@ealphabits.com>
2023-10-05 20:31:24 +0530

    initial-commit

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+
+@Controller('ninjas')
+export class NinjasController {
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  @Put('id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      name: updateNinjaDto,
+    };
+  }
+
+  @Delete('id')
+  removeNinja() {
+    return {};
+  }
+}
